b17666adcc87dd329fd1a3a9a2f433108998d7db
YafaKh <103255894+YafaKh@users.noreply.github.com>
2025-05-16 10:39:49 +0300

    Create stacks.dart

---
+++ stacks.dart
@@ -0,0 +1,19 @@
+void reverseList(List l) {
+  List stack = [];
+
+  for (var i in l) {
+    stack.add(i);
+  }
+  
+  print('Reversed List:');
+
+  while (stack.isNotEmpty) {
+    print(stack.removeLast());
+  }
+}
+
+void main() {
+  List<int> numbers = [20,30,40,50];
+  reverseList(numbers);
+}
+
